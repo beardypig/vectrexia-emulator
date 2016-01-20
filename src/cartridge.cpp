5b0f93e090a22421f066d5cab0cdf0b466967b6a
beardypig <beardypig@users.noreply.github.com>
2016-01-20 13:49:20 +0000

    typo in cartridge.h include

--- src/cartridge.cpp
+++ src/cartridge.cpp
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Vectrexia.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "Cartridge.h"
+#include "cartridge.h"
 #include <cstring>
 
 void Cartridge::Load(const uint8_t *data, size_t size)
