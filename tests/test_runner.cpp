18f6b7191fc1fdbd33bc1f97af801b2b56892f52
beardypig <beardypig@users.noreply.github.com>
2016-01-31 22:14:44 +0000

    set numeric locale

--- tests/test_runner.cpp
+++ tests/test_runner.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
+#include <locale.h>
 
 int main(int argc, char* argv[])
 {
+    setlocale(LC_NUMERIC, "");
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
