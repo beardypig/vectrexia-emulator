dfbbf20158a7a92ee01a57df28503625fa1773f5
beardypig <beardypig@users.noreply.github.com>
2016-02-11 15:49:00 +0000

    add vectrex callbacks for reading the joystick

--- src/vectrexia.cpp
+++ src/vectrexia.cpp
@@ -66,6 +66,10 @@ uint64_t Vectrex::Run(uint64_t cycles)
         for (int via_cycles = 0; via_cycles < cpu_cycles; via_cycles++)
         {
             via_->Step();
+
+            UpdateJoystick(via_->getPortAState(), via_->getPortBState());
+            psg_->Step(via_->getPortAState(), (uint8_t) ((via_->getPortBState() >> 3) & 1),
+                       1, (uint8_t) ((via_->getPortBState() >> 4) & 1));
             this->cycles++;
         }
 
@@ -100,17 +104,33 @@ static void write_mem(intptr_t ref, uint16_t addr, uint8_t data)
     reinterpret_cast<Vectrex*>(ref)->Write(addr, data);
 }
 
+static uint8_t read_psg_io(intptr_t ref)
+{
+    return reinterpret_cast<Vectrex*>(ref)->ReadPSGIO();
+}
+
+static void store_psg_reg(intptr_t ref, uint8_t data)
+{
+    reinterpret_cast<Vectrex*>(ref)->StorePSGReg(data);
+}
+
 Vectrex::Vectrex()
 {
     cpu_ = std::make_unique<M6809>();
     via_ = std::make_unique<VIA6522>();
+    psg_ = std::make_unique<AY38910>();
+
+    // CPU Callbacks
     cpu_->SetReadCallback(read_mem, reinterpret_cast<intptr_t>(this));
     cpu_->SetWriteCallback(write_mem, reinterpret_cast<intptr_t>(this));
+
+    // PSG callbacks
+    psg_->SetIOReadCallback(read_psg_io, reinterpret_cast<intptr_t>(this));
+    psg_->SetRegStoreCallback(store_psg_reg, reinterpret_cast<intptr_t>(this));
 }
 
 uint8_t Vectrex::Read(uint16_t addr)
 {
-    printf("Reading from addr: $%04x\n", addr);
     // 0000-7FFF: cartridge
     if (addr < 0x8000 && cartridge_) {
         return cartridge_->Read(addr);
@@ -171,4 +191,89 @@ void Vectrex::message(const char *fmt, ...)
     vfprintf(stderr, fmt, args);
     fprintf(stderr, "\n");
     va_end(args);
+}
+
+
+// function pointers for when PORTA/B are read
+uint8_t Vectrex::ReadPortA() {
+    return psg_port;
+}
+
+uint8_t Vectrex::ReadPortB() {
+    return joystick_compare;
+}
+
+void Vectrex::UpdateJoystick(uint8_t porta, uint8_t portb) {
+    // porta is connected to the databus of the sound chip and DAC
+    // portb 3+4 and ca1 for sound chip stuff
+
+    // PB0 - S/H
+    // PB1 - SEL 0
+    // PB2 - SEL 1
+    // PB5 - COMPARE (input)
+    // PB6 - CART N/C?
+    // PB7 - RAMP
+    uint8_t select, pot;
+    select = (uint8_t) ((portb >> 1) & 0x3);
+
+    // S/H enables the demultiplexor
+    // select selects the channel for the comparator
+    switch (select) {
+        case 0:
+            pot = p1_joystick.pot_x;
+            break;
+        case 1:
+            pot = p1_joystick.pot_y;
+            break;
+        case 2:
+            pot = p2_joystick.pot_x;
+            break;
+        case 3:
+            pot = p2_joystick.pot_y;
+            break;
+        default:
+            pot = 0x7f;
+    }
+
+    // compare the value on porta to the selected joystick pot value
+    joystick_compare = (uint8_t) ((pot > (porta ^ 0x80)) ? 0x20 : 0);
+}
+
+void Vectrex::SetPlayerOne(uint8_t x, uint8_t y, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4)
+{
+    p1_joystick.pot_x = x;
+    p1_joystick.pot_y = y;
+
+    p1_joystick.btn_1 = b1;
+    p1_joystick.btn_2 = b2;
+    p1_joystick.btn_3 = b3;
+    p1_joystick.btn_4 = b4;
+}
+
+void Vectrex::SetPlayerTwo(uint8_t x, uint8_t y, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4)
+{
+    p2_joystick.pot_x = x;
+    p2_joystick.pot_y = y;
+
+    p2_joystick.btn_1 = b1;
+    p2_joystick.btn_2 = b2;
+    p2_joystick.btn_3 = b3;
+    p2_joystick.btn_4 = b4;
+}
+
+uint8_t Vectrex::ReadPSGIO()
+{
+    return (p2_joystick.btn_4 << 7) | \
+           (p2_joystick.btn_3 << 6) | \
+           (p2_joystick.btn_2 << 5) | \
+           (p2_joystick.btn_1 << 4) | \
+           (p1_joystick.btn_4 << 3) | \
+           (p1_joystick.btn_3 << 2) | \
+           (p1_joystick.btn_2 << 1) | \
+           (p1_joystick.btn_1);
+}
+
+void Vectrex::StorePSGReg(uint8_t reg)
+{
+    psg_port = reg;
 }
