e7b43d5505a1488b58097d4b2b56f93316f9c5de
beardypig <beardypig@protonmail.com>
2018-08-08 16:25:07 +0200

    fix test discovery

--- tests/test_runner.cpp
+++ tests/test_runner.cpp
@@ -1,9 +1,11 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <locale.h>
 
 int main(int argc, char* argv[])
 {
     setlocale(LC_NUMERIC, "");
     testing::InitGoogleTest(&argc, argv);
+    testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
 }
