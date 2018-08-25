91d70e029b43e95aaa56d2036a1dd20789757c25
pelorat <f.berglund+github@gmail.com>
2018-08-25 17:21:17 +0200

    Added some rect_t tests

--- tests/gfxutil_test.cpp
+++ tests/gfxutil_test.cpp
@@ -48,3 +48,62 @@ TEST(ARGB, TestExtractChannelARGB)
   EXPECT_EQ(argb_pixel.comp_g(argb_pixel), 0x20);
   EXPECT_EQ(argb_pixel.comp_b(argb_pixel), 0x30);
 }
+
+TEST(GFXUtil, RectIntersect)
+{
+    auto a = vxgfx::rect_t(10, 10);
+    auto b = vxgfx::rect_t(-2, -2, 14, 14);
+    auto c = vxgfx::rect_t(-1, -1, 6,  6);
+    auto d = vxgfx::rect_t( 5, -1, 6,  6);
+    auto e = vxgfx::rect_t( 5,  5, 6,  6);
+    auto f = vxgfx::rect_t(-1,  5, 6,  6);
+
+    EXPECT_EQ(vxgfx::intersect(a, b), vxgfx::rect_t(0, 2, 10, 8));
+    EXPECT_EQ(vxgfx::intersect(a, c), vxgfx::rect_t(0, 0, 5, 5));
+    EXPECT_EQ(vxgfx::intersect(a, d), vxgfx::rect_t(5, 0, 5, 5));
+    EXPECT_EQ(vxgfx::intersect(a, e), vxgfx::rect_t(5, 5, 5, 5));
+    EXPECT_EQ(vxgfx::intersect(a, f), vxgfx::rect_t(0, 5, 5, 5));
+}
+
+TEST(GFXUtil, RectArea)
+{
+    auto a = vxgfx::rect_t(10, 10);
+
+    EXPECT_EQ(a.area(), 100);
+}
+
+TEST(GFXUtil, RectBoolean)
+{
+    auto a = vxgfx::rect_t(10, 10);
+    auto b = vxgfx::rect_t(0, 0);
+
+    EXPECT_TRUE(a);
+    EXPECT_FALSE(b);
+}
+
+TEST(GFXUtil, RectMove)
+{
+    auto a = vxgfx::rect_t(10, 10);
+    a.move(10, 10);
+
+    EXPECT_EQ(a, vxgfx::rect_t(10, 10, 10, 10));
+}
+
+TEST(GFXUtil, RectOffset)
+{
+    auto a = vxgfx::rect_t(10, 10, 10, 10);
+    a.offset(-10, -10);
+
+    EXPECT_EQ(a, vxgfx::rect_t(0, 0, 10, 10));
+}
+
+TEST(GFXUtil, RectInit)
+{
+    auto a = vxgfx::rect_t();
+    auto b = vxgfx::rect_t(10,10);  
+    auto c = vxgfx::rect_t(10,10,10,10);
+    
+    EXPECT_TRUE(a.left == 0 && a.top == 0 && a.right == 0 && a.bottom == 0);
+    EXPECT_TRUE(b.left == 0 && b.top == 0 && b.right == 10 && b.bottom == 10);
+    EXPECT_TRUE(c.left == 10 && c.top == 10 && c.right == 20 && c.bottom == 20);
+}
