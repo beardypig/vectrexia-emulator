f630e3ccbe845aaaef25e189e160078efacbb3e7
pelorat <f.berglund+github@gmail.com>
2018-11-08 00:22:50 +0100

    multiplexer changes

--- src/vectorizer.cpp
+++ src/vectorizer.cpp
@@ -9,38 +9,53 @@ constexpr float normalise(float value, float min, float max) {
   return (value - min) / (max - min);
 }
 
+float dac_(uint8_t value)
+{
+    return 2.5f - ((value ^ 0x80) * (5.0f / 256.0f));
+}
+
 void Vectorizer::Step(uint8_t porta, uint8_t portb, uint8_t zero, uint8_t blank)
 {
-  // porta is connected to the databus of the sound chip and DAC
+    // porta is connected to the databus of the sound chip and DAC
 
-  // PB0 - SWITCH
-  // PB1 - SEL 0
-  // PB2 - SEL 1
-  // PB5 - COMPARE (input)
-  // PB6 - CART Mapper
-  // PB7 - RAMP
+    // PB0 - SWITCH
+    // PB1 - SEL 0
+    // PB2 - SEL 1
+    // PB5 - COMPARE (input)
+    // PB6 - CART Mapper
+    // PB7 - RAMP
 
-  dac.v = (float)porta * (10.0f/256.0f) - 5.0f;
+    // X input
+    dac.v = dac_(porta);
 
-  via.blank = blank == 0;
-  via.zero = zero == 0;
-  via.ramp = ((portb >> 7u) & 0x01u) == 0;
+    uint8_t switch_ = (uint8_t)(portb & 0x1);
+    uint8_t select = (uint8_t)((portb >> 1) & 0x3);
 
-  // Multiplexer
-  mpx.reset();
-  if ((portb & 0x1u) == 0) { // if the mpx is enabled
-    auto sel = (portb >> 1u) & 0x3u;
-    if (sel == 0) {
-      mpx.out1A = dac.v;  // Y Buffer
-    } else if (sel == 1) {
-      mpx.out1B = dac.v;  // integrator offset
-    } else if (sel == 2) {
-      mpx.out1C = dac.v;  // Z Buffer
+    via.blank = (blank == 0);
+    via.zero  = (zero == 0);
+    via.ramp  = (uint8_t)portb >> 7 == 0;
+
+    if (!switch_)
+    {
+        switch (select)
+        {
+        case 0: // Y Axis Sample and Hold between [-5, +5]
+            mpx.out1A = dac.v * 2;
+            break;
+        case 1: // Zero reference
+            mpx.out1B = dac.v * 2;
+            break;
+        case 2: // Z Axis (brightness) Sample and Hold
+            mpx.out1C = std::max(0.0f, -dac.v * 2);
+            break;
+        default:
+            break;
+        }
     }
-  }
 
   auto X0 = static_cast<float>(integrators.vX);
   auto Y0 = static_cast<float>(integrators.vY);
+
   integrators.step();
 
   auto intensity = normalise(static_cast<float>(integrators.vZ), -5.0f, +5.0f);
