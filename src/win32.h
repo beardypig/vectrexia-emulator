b33b98e0cb19e130b713e9114df97fc2e742de0e
pelorat <f.berglund+github@gmail.com>
2024-10-19 12:33:55 +0200

    OCD-fix for the GPL header

--- src/win32.h
+++ src/win32.h
@@ -4,17 +4,17 @@ Copyright (C) 2016 pelorat
 This file is part of Vectrexia.
 
 Vectrexia is free software: you can redistribute it and/or modify
-        it under the terms of the GNU General Public License as published by
+it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
 
 Vectrexia is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Vectrexia.  If not, see <http://www.gnu.org/licenses/>.
+along with Vectrexia. If not, see <http://www.gnu.org/licenses/>.
 */
 #ifndef WIN32_H
 #define WIN32_H
