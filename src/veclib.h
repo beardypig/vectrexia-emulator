21e7059f50eddb98fd489e70533e97fa3b793b2f
beardypig <git@beardypig.com>
2024-10-20 03:18:03 +0200

    Build process modernisation (#13)
    Switched to vcpkg for package management.
    Switched from GTtest/GMock to Catch2 and Trompeloeil.
    Using CMake presets.
    presets for MSVC and ninja
    
    Co-authored-by: pelorat <f.berglund+github@gmail.com>

--- src/veclib.h
+++ src/veclib.h
@@ -4,17 +4,17 @@ Copyright (C) 2016 beardypig, pelorat
 This file is part of Vectrexia.
 
 Vectrexia is free software: you can redistribute it and/or modify
-        it under the terms of the GNU General Public License as published by
+it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
 
 Vectrexia is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Vectrexia.  If not, see <http://www.gnu.org/licenses/>.
+along with Vectrexia. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef VECTREXIA_VECLIB_H
@@ -44,7 +44,7 @@ constexpr const T& clamp(const T& v, const T& lo, const T& hi, Compare comp) {
 
 template<class T>
 constexpr const T& clamp(const T& v, const T& lo, const T& hi) {
-    return clamp(v, lo, hi, std::less<>());
+    return vxl::clamp(v, lo, hi, std::less<>());
 }
 
 inline std::string format(const char *fmt, ...)
